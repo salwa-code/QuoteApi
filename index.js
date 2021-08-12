0c559eb5669b6c617887659935c2d52a8aa43fe8
salwahamidah <salwahamidah00@gmail.com>
2021-08-12 09:01:50 +0300

    initial commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // Quote Route
-const quoteRoute = require('./routes/quoteRoute');
+const quoteRoute = require('./routes/quoteRoute.js');
 
 const app = express();
 
@@ -11,23 +11,35 @@ const app = express();
 // Middleware - helps modify the request
 app.use(express.json());
 
+//CORS
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, authorization');
+	next();
+});
+
 // Routes for API
+app.get('/',(req,res)=>{
+    res.send('Welcome to the Quote API')
+})
+
 app.use('/quotes',quoteRoute)
 
 
 
 // Connect to Our Database
 const URL = process.env.DB_URL;
+const PORT = process.env.PORT || 4040;
 mongoose.connect(URL,{ useNewUrlParser: true, useUnifiedTopology: true })
     .then(()=>{
         console.log('Database Connection Established...');
-
-        // Start the Server
-        app.listen(3000,()=>{
-            console.log('Server is listening on port 3000 ...')
-        })
     })
     .catch(()=>{
         console.log('Database Connection Failed !!')
     })
 
+// Start the Server
+app.listen(PORT,()=>{
+    console.log('Server is listening on port 4040 ...')
+})
